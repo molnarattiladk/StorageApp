649c4693604a2f1180b794d3553cbae308c38150
Molnár Attila <molnar.attila.dk@gmail.com>
2019-02-26 13:58:15 +0100

    Init

---
+++ StorageApp.API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using StorageApp.API.Models;
+
+namespace StorageApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base (options)
+        {
+            
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
