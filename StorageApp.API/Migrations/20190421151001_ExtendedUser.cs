5f94a2fcfae6ba55f6abd7be8106e115d4ff0dae
Molnár Attila <molnar.attila.dk@gmail.com>
2019-04-21 18:26:42 +0200

    update user class

---
+++ StorageApp.API/Migrations/20190421151001_ExtendedUser.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StorageApp.API.Migrations
+{
+    public partial class ExtendedUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Contact",
+                table: "Users",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "OnHoliday",
+                table: "Users",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Role",
+                table: "Users",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Salary",
+                table: "Users",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Started",
+                table: "Users",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Contact",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "OnHoliday",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Role",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Salary",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Started",
+                table: "Users");
+        }
+    }
+}
--- StorageApp.API/Models/User.cs
+++ StorageApp.API/Models/User.cs
@@ -1,5 +1,8 @@
+using System;
+
 namespace StorageApp.API.Models
 {
+    public enum Role{ADMIN, BOSS, EMPLOYE}
     public class User
     {
         
@@ -7,6 +10,16 @@ namespace StorageApp.API.Models
         public string UserName { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+
+        public Role Role { get; set; }
+
+        public double Salary { get; set; }
+
+        public bool OnHoliday { get; set; }
+
+        public DateTime Started { get; set; }
+
+        public string Contact { get; set; }
         
     }
 }
